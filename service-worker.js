c4b0f96044fe5b0de095fe6a3ff62d69bf60d234
dl-pm <makoto.yamamoto@dartslive.com>
2026-07-07 12:15:42 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='kore-v3';
+const CACHE='kore-v4';
 const ASSETS=['./','./←KORE.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
